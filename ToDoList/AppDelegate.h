d79fba6fef9662dfd22c04783f8798aacb53d7e5
Neeraj <Neeraj@neerajs-macbook.local>
2014-06-04 17:53:38 +0530

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Neeraj on 6/4/14.
+//  Copyright (c) 2014 SubtleCreation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
